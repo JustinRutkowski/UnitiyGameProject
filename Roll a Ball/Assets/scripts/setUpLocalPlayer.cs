4db4f36414103cfd833a83eb6b715452ab5a1253
JustinRutkowski <justin.rutkowski@mailbox.tu-dresden.de>
2017-12-04 13:49:16 +0100

    Multiplayer Experiment on Roll a Ball sample

---
+++ Roll a Ball/Assets/scripts/setUpLocalPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class setUpLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+            GetComponent<PlayerController>().enabled = true;	
+	}	
+}
