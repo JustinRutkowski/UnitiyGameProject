d5b6c34fcdabdccd8445cd8123482aab2c164cb5
Gabel95 <gabriel2giese@gmail.com>
2017-12-04 18:51:55 +0100

    Connected successfully to the Master. c:

---
+++ Random Retards/Assets/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.PunBehaviour
+{
+    [SerializeField] private Text connectText;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        //FOR TESTING ONLY
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
