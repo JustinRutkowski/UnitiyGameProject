986d92e78e867b326f8e09c7977285d31af6ebc1
Gabel95 <gabriel2giese@gmail.com>
2017-12-05 01:28:30 +0100

    Network Test 2

---
+++ Random Retards/Assets/PlayerNetwork.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private MonoBehaviour[] playerControlScripts;
+
+    private PhotonView photonview;
+
+    private void Start()
+    {
+        photonview = GetComponent<PhotonView>();
+
+        initialize();
+    }
+
+    private void initialize()
+    {
+        if (photonview.isMine)
+        {
+
+        }
+        //handle functionality for non local player
+        else
+        {
+            //disable its camera
+            playerCamera.SetActive(false);
+            //disable its control scripts
+            foreach (MonoBehaviour m in playerControlScripts)
+            {
+                m.enabled = false;
+            }
+        }
+    }
+}
